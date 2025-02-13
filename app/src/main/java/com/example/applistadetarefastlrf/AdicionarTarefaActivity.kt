4a121597556a3e5d10eaacfc30b454e5245221c6
Thiago Freitas <thiagoleonardo1995@live.com>
2025-02-12 22:17:18 -0300

    Criando Interface

---
+++ app/src/main/java/com/example/applistadetarefastlrf/AdicionarTarefaActivity.kt
@@ -0,0 +1,21 @@
+package com.example.applistadetarefastlrf
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.applistadetarefastlrf.databinding.ActivityAdicionarTarefaBinding
+import com.example.applistadetarefastlrf.databinding.ActivityMainBinding
+
+class AdicionarTarefaActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityAdicionarTarefaBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
