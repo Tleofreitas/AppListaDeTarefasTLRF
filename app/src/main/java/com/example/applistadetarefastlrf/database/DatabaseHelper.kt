93a5c5075da71b90f81f639bf3b6710e36c4db22
Thiago Freitas <thiagoleonardo1995@live.com>
2025-02-12 22:38:11 -0300

    Banco de Dados: criação da tabela

--- app/src/main/java/com/example/applistadetarefastlrf/database/DatabaseHelper.kt
+++ app/src/main/java/com/example/applistadetarefastlrf/database/DatabaseHelper.kt
@@ -3,6 +3,7 @@ package com.example.applistadetarefastlrf.database
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
 
 class DatabaseHelper(context: Context) : SQLiteOpenHelper(
     context, NOME_BANCO_DE_DADOS, null, VERSAO
@@ -10,10 +11,27 @@ class DatabaseHelper(context: Context) : SQLiteOpenHelper(
     companion object {
         const val NOME_BANCO_DE_DADOS = "ListaTarefas.db"
         const val VERSAO = 1
+
+        const val TABELA_TAREFAS = "tarefas"
+
+        //Colunas tabela tarefas
+        const val ID_TAREFA = "id_tarefa"
+        const val DESCRICAO = "descricao"
+        const val DATA_CRIACAO = "data_criacao"
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
-        TODO("Not yet implemented")
+        val sqlTabela = "CREATE TABLE IF NOT EXISTS $TABELA_TAREFAS (" +
+                "$ID_TAREFA INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                "$DESCRICAO VARCHAR(70) NOT NULL," +
+                "$DATA_CRIACAO DATETIME DEFAULT CURRENT_TIMESTAMP" +
+                ");"
+        try {
+            db?.execSQL( sqlTabela )
+            Log.i("info_db", "Sucesso ao criar a tabela")
+        } catch (e: Exception){
+            Log.i("info_db", "erro ao criar tabela ${e.message}")
+        }
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
---
+++ app/src/main/java/com/example/applistadetarefastlrf/model/Tarefa.kt
@@ -0,0 +1,7 @@
+package com.example.applistadetarefastlrf.model
+
+data class Tarefa (
+    val idTarefa: Int,
+    val descricao: String,
+    val dataCadastro: String
+)
